9de9a1f0ed8e7510f666261bab90740d2c143744
Siddharth <siddharth201820@gmail.com>
2021-07-30 19:25:33 +0530

    completed Day 28

--- src/Day 28/Weather.js
+++ src/Day 28/Weather.js
@@ -1,16 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
 
 const Weather = () => {
-  useEffect(() => {
+  const [place, setPlace] = useState("");
+  const [placeData, setPlaceData] = useState({});
+  const updatePlaceData = () => {
     fetch(
-      "https://api.weatherapi.com/v1/current.json?key=45c670119c8a4b45a8b144153212707&q=Mumbai"
+      `https://api.weatherapi.com/v1/current.json?key=45c670119c8a4b45a8b144153212707&q=${place}`
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setPlaceData(data);
       });
-  }, []);
-  return <div>hello world</div>;
+  };
+  return (
+    <div className="weather-app">
+      <div className="container weather">
+        <div className="row">
+          <div className="col-12 form">
+            <input
+              type="text"
+              value={place}
+              onChange={(e) => setPlace(e.target.value)}
+            />
+            <button className="btn btn-primary" onClick={updatePlaceData}>
+              Submit
+            </button>
+          </div>
+          <div className="offset-md-4 col-12 col-md-4 weather">
+            <div className="card w-100 text-center">
+              {placeData.location ? (
+                <div>
+                  <img src={placeData.current.condition.icon} alt="" />
+                  <div className="temp">{placeData.current.temp_c}&deg;</div>
+                  <div className="desc">{placeData.current.condition.text}</div>
+                  <div className="place">{placeData.location.name}</div>
+                  <div className="container">
+                    <div className="row whp">
+                      <div className="col">
+                        <div className="title">Wind Now</div>
+                        <div className="data">
+                          {placeData.current.wind_kph}
+                          <span className="unit">KM</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Humidity</div>
+                        <div className="data">
+                          {placeData.current.humidity}
+                          <span className="unit">%</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Precipitation</div>
+                        <div className="data">
+                          {placeData.current.precip_in}
+                          <span className="unit">%</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <h2>Place Not Found</h2>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
