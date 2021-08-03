b3b6f0829195959383a6f32ddc3d3e2d340a3d96
Siddharth <siddharth201820@gmail.com>
2021-08-03 22:41:48 +0530

    bug fixes in day 27

--- src/Day 27/todoStore.js
+++ src/Day 27/todoStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../Day 27/reducers";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
